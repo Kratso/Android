73a612342e7e8f36de96a1f697f24fb972538b16
Kratso <kratsothelsamar@gmail.com>
2018-11-07 12:49:41 +0100

    cosas marinas

--- CosasDePecesDeEsasDeTal/app/src/main/java/laz/mateo/notcom/com/cosasdepecesdeesasdetal/MainActivity.java
+++ CosasDePecesDeEsasDeTal/app/src/main/java/laz/mateo/notcom/com/cosasdepecesdeesasdetal/MainActivity.java
@@ -1,14 +1,15 @@
 package laz.mateo.notcom.com.cosasdepecesdeesasdetal;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -16,21 +17,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
     //Declaramos dos ArrayList que van a ser utilizados para manipular el contenido del ListView.
     ArrayList<FMarina> peces = new ArrayList<>();
-    ArrayList <FMarina> alginvert = new ArrayList<>();
+    ArrayList<FMarina> alginvert = new ArrayList<>();
     //Guardamos en una variable la actividad actual.
     final Activity a = this;
     //Se declara el adaptador del ListView.
     AdapterFMarina adaptador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Declaramos dos objetos, uno de tipo ListView y otro de tipo Spinner, para manipular el contenido de la app.
-        final ListView lista = (ListView)findViewById(R.id.miLista);
+        final ListView lista = (ListView) findViewById(R.id.miLista);
         final Spinner spinner = (Spinner) findViewById(R.id.miSpinner);
         //Se rellena el Spinner.
         contenidoSpinner(spinner);
@@ -38,24 +42,26 @@ public class MainActivity extends AppCompatActivity {
         rellenarPeces();
         rellenarAlgInver();
         //Se establece el adaptador por defecto del ListView.
-        adaptador = new AdapterFMarina(this,peces);
+        adaptador = new AdapterFMarina(this, peces);
         lista.setAdapter(adaptador);
+
         //Si se cambia la opción del Spinner:
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                //Si la opcion es "Peces" se borrará el contenido del ListView y se establecerá como su nuevo contenido el correspondiente al ArrayList "peces".
-                if(position==0){
-                    adaptador = new AdapterFMarina(a,peces);
+                TextView tv = findViewById(R.id.editText);
+                if (position == 0) {
+                    adaptador = new AdapterFMarina(a, peces);
                     lista.setAdapter(adaptador);
+                    tv.setText("Peces del Parque");
                 }
-                //Si la opcion es "Algas e invertebrados" se borrará el contenido del ListView y se establecerá como su nuevo contenido el correspondiente al ArrayList "alginvert".
-                else
-                {
-                    adaptador = new AdapterFMarina(a,alginvert);
+                else {
+                    adaptador = new AdapterFMarina(a, alginvert);
                     lista.setAdapter(adaptador);
+                    tv.setText("Algas e Invertebrados del Parque");
                 }
             }
+
             @Override
             //Si no se selecciona nada en el Spinner, no se hará nada.
             public void onNothingSelected(AdapterView<?> parentView) {
@@ -66,74 +72,69 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 //Se llamará a una segunda actividad, a la que se enviará la referencia numérica de la imagen del item del ListView.
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-                intent.putExtra("ref",adaptador.getItem(i).getRef());
-                startActivityForResult(intent,SECONDARY_ACTIVITY_TAG);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("ref", adaptador.getItem(i).getRef());
+                startActivityForResult(intent, SECONDARY_ACTIVITY_TAG);
             }
         });
+
     }
 
     //Método que inserta dos valores predeterminados en un spinner.
-    public void contenidoSpinner(Spinner s){
-        String [] array = {"Peces","Algas e invertebrados"};
+    public void contenidoSpinner(Spinner s) {
+        String[] array = {"Peces", "Algas e invertebrados"};
         s.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array));
     }
 
     //Método que rellenar el ArrayList peces con los valores del txt correspondiente.
-    public void rellenarPeces(){
-        String linea="ERROR.";
-        try
-        {
+    public void rellenarPeces() {
+        String linea = "ERROR.";
+        try {
             InputStream fraw =
                     getResources().openRawResource(R.raw.peces);
 
             BufferedReader brin =
                     new BufferedReader(new InputStreamReader(fraw));
-            linea=brin.readLine();
-            while (linea!=null){
-                String [] splitter= linea.split(",");
-                peces.add(new FMarina(asignarImagen(splitter[0]),splitter[1],splitter[2],splitter[3],splitter[4]));
+            linea = brin.readLine();
+            while (linea != null) {
+                String[] splitter = linea.split(",");
+                peces.add(new FMarina(asignarImagen(splitter[0]), splitter[1], splitter[2], splitter[3], splitter[4]));
 
                 linea = brin.readLine();
             }
             brin.close();
             fraw.close();
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Toast.makeText(this, linea, Toast.LENGTH_SHORT).show();
         }
     }
 
     //Método que rellenar el ArrayList alginvert con los valores del txt correspondiente.
-    public void rellenarAlgInver(){
-        String linea="ERROR.";
-        try
-        {
+    public void rellenarAlgInver() {
+        String linea = "ERROR.";
+        try {
             InputStream fraw =
                     getResources().openRawResource(R.raw.algaseinvertebrados);
 
             BufferedReader brin =
                     new BufferedReader(new InputStreamReader(fraw));
 
-            linea=brin.readLine();
-            while (linea!=null){
-                String [] splitter= linea.split(",");
-                alginvert.add(new FMarina(asignarImagen(splitter[0]),splitter[1],splitter[2],splitter[3],splitter[4]));
+            linea = brin.readLine();
+            while (linea != null) {
+                String[] splitter = linea.split(",");
+                alginvert.add(new FMarina(asignarImagen(splitter[0]), splitter[1], splitter[2], splitter[3], splitter[4]));
                 linea = brin.readLine();
             }
             brin.close();
             fraw.close();
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             Toast.makeText(this, linea, Toast.LENGTH_SHORT).show();
         }
     }
 
     //Método que devuelve la referencia a una imagen según el String que reciba.
-    public int asignarImagen(String nombre){
-        switch(nombre){
+    public int asignarImagen(String nombre) {
+        switch (nombre) {
             case "anemona":
                 return R.drawable.anemona;
             case "bigaro":
@@ -237,21 +238,17 @@ public class MainActivity extends AppCompatActivity {
         return 0;
     }
 
-    //Método que gestiona el flujo entre antividades de la app.
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        String respuesta="";
+        String respuesta;
 
-        //Si se cancela la operación de la actividad secundaria se mostrará un mensaje informativo de tipo Toast.
-        if (resultCode == RESULT_CANCELED){
+        if (resultCode == RESULT_CANCELED) {
             respuesta = "Has salido de la vista en pantalla completa.";
-            Toast toast1 = Toast.makeText(getApplicationContext(),respuesta, Toast.LENGTH_SHORT);
+            Toast toast1 = Toast.makeText(getApplicationContext(), respuesta, Toast.LENGTH_SHORT);
             toast1.show();
         }
-        else {
-
-        }
     }
+
     private static final int SECONDARY_ACTIVITY_TAG = 1;
 }
--- CosasDePecesDeEsasDeTal/app/src/main/java/laz/mateo/notcom/com/cosasdepecesdeesasdetal/MainActivity2.java
+++ CosasDePecesDeEsasDeTal/app/src/main/java/laz/mateo/notcom/com/cosasdepecesdeesasdetal/MainActivity2.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+public class MainActivity2 extends AppCompatActivity {
 
-public class MainActivity2 extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagengrande);
         //Se recoge el valor numérico que devuelve la actividad principal y se asigna la imagen correspondiente al valor en el objeto ImageView de la actividad.
-        int num = getIntent().getIntExtra("ref",0);
-        ImageView im = (ImageView)findViewById(R.id.imageView2);
+        int num = getIntent().getIntExtra("ref", 0);
+        ImageView im = (ImageView) findViewById(R.id.imageView2);
         im.setImageResource(num);
     }
 }
